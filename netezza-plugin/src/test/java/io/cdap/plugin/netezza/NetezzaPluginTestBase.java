57bb68dac0af2b376429f3dcd5422ae461ad9137
Vlad Hlinskiy <vladhlinskiy@gmail.com>
2019-06-26 20:35:15 +0300

    CDAP-15550 Netezza db plugin enhacements: all data types support + proper test coverage

--- netezza-plugin/src/test/java/io/cdap/plugin/netezza/NetezzaPluginTestBase.java
+++ netezza-plugin/src/test/java/io/cdap/plugin/netezza/NetezzaPluginTestBase.java
@@ -20,6 +20,7 @@ import com.google.common.base.Charsets;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Sets;
+
 import io.cdap.cdap.api.artifact.ArtifactSummary;
 import io.cdap.cdap.api.plugin.PluginClass;
 import io.cdap.cdap.datapipeline.DataPipelineApp;
@@ -31,20 +32,25 @@ import io.cdap.plugin.db.DBRecord;
 import io.cdap.plugin.db.batch.DatabasePluginTestBase;
 import io.cdap.plugin.db.batch.sink.ETLDBOutputFormat;
 import io.cdap.plugin.db.batch.source.DataDrivenETLDBInputFormat;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Map;
@@ -59,7 +65,7 @@ public class NetezzaPluginTestBase extends DatabasePluginTestBase {
 
   protected static String connectionUrl;
   protected static final int YEAR;
-  protected static final int PRECISION = 10;
+  protected static final int PRECISION = 16;
   protected static final int SCALE = 6;
   protected static boolean tearDown = true;
   private static int startCount;
@@ -91,9 +97,9 @@ public class NetezzaPluginTestBase extends DatabasePluginTestBase {
     setupBatchArtifacts(DATAPIPELINE_ARTIFACT_ID, DataPipelineApp.class);
 
     addPluginArtifact(NamespaceId.DEFAULT.artifact(JDBC_DRIVER_NAME, "1.0.0"),
-                      DATAPIPELINE_ARTIFACT_ID,
-                      NetezzaSource.class, NetezzaSink.class, DBRecord.class, ETLDBOutputFormat.class,
-                      DataDrivenETLDBInputFormat.class, DBRecord.class, NetezzaPostAction.class, NetezzaAction.class);
+      DATAPIPELINE_ARTIFACT_ID,
+      NetezzaSource.class, NetezzaSink.class, DBRecord.class, ETLDBOutputFormat.class,
+      DataDrivenETLDBInputFormat.class, DBRecord.class, NetezzaPostAction.class, NetezzaAction.class);
 
     connectionUrl = "jdbc:netezza://" + BASE_PROPS.get(ConnectionConfig.HOST) + ":" +
       BASE_PROPS.get(ConnectionConfig.PORT) + "/" + BASE_PROPS.get(ConnectionConfig.DATABASE);
@@ -102,11 +108,11 @@ public class NetezzaPluginTestBase extends DatabasePluginTestBase {
 
     // add netezza 3rd party plugin
     PluginClass netezzaDriver = new PluginClass(ConnectionConfig.JDBC_PLUGIN_TYPE, JDBC_DRIVER_NAME,
-                                                "netezza driver class", aClass.getCanonicalName(),
-                                                null, Collections.emptyMap());
+      "netezza driver class", aClass.getCanonicalName(),
+      null, Collections.emptyMap());
     addPluginArtifact(NamespaceId.DEFAULT.artifact("netezza-jdbc-connector", "1.0.0"),
-                      DATAPIPELINE_ARTIFACT_ID,
-                      Sets.newHashSet(netezzaDriver), aClass);
+      DATAPIPELINE_ARTIFACT_ID,
+      Sets.newHashSet(netezzaDriver), aClass);
 
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 
@@ -122,92 +128,64 @@ public class NetezzaPluginTestBase extends DatabasePluginTestBase {
       // create a table that the action will truncate at the end of the run
       stmt.execute("CREATE TABLE post_action_test (x int, day varchar(10))");
 
-      stmt.execute("CREATE TABLE my_table (" +
-                     "ID INT NOT NULL," +
-                     "NAME VARCHAR(40) NOT NULL," +
-                     "SCORE REAL," +
-                     "GRADUATED BOOLEAN," +
-                     "NOT_IMPORTED VARCHAR(30)," +
-                     "BYTEINT_COL BYTEINT," +
-                     "SMALLINT_COL SMALLINT," +
-                     "INTEGER_COL INTEGER," +
-                     "BIGINT_COL BIGINT," +
-                     "CHAR_COL CHARACTER(40)," +
-                     "VARCHAR_COL CHARACTER VARYING(40)," +
-                     "NCHAR_COL NATIONAL CHARACTER(40)," +
-                     "DATE_COL DATE," +
-                     "TIME_COL TIME," +
-                     "TIME_WITH_TIME_ZONE_COL TIME WITH TIME ZONE," +
-                     "TIMESTAMP_COL TIMESTAMP," +
-                     "INTERVAL_COL INTERVAL," +
-                     "DOUBLE_PRECISION_COL DOUBLE PRECISION," +
-                     "NUMERIC_COL NUMERIC(" + PRECISION + "," + SCALE + ")," +
-                     "NVARCHAR_COL NATIONAL CHARACTER VARYING(40)," +
-                     "REAL_COL REAL," +
-                     "ST_GEOMETRY_COL ST_GEOMETRY(10)," +
-                     "VARBINARY_COL BINARY VARYING(10)," +
-                     "DECIMAL_COL DECIMAL(" + PRECISION + "," + SCALE + ")," +
-                     "FLOAT_COL FLOAT(6))");
-      stmt.execute("CREATE TABLE MY_DEST_TABLE AS " +
-                     "SELECT * FROM my_table");
-      stmt.execute("CREATE TABLE your_table AS " +
-                     "SELECT * FROM my_table");
+      stmt.execute("create table MY_TABLE (INTEGER_COL INTEGER, BYTEINT_COL BYTEINT, SMALLINT_COL SMALLINT, " +
+        "BIGINT_COL BIGINT, REAL_COL REAL, REAL_FLOAT_COL FLOAT(1), DOUBLE_FLOAT_COL FLOAT(7), " +
+        "DOUBLE_PRECISION_COL DOUBLE PRECISION, NUMERIC_COL NUMERIC(" + PRECISION + "," + SCALE + "), " +
+        "DECIMAL_COL DECIMAL(" + PRECISION + "," + SCALE + "), CHAR_COL CHAR(40), VARCHAR_COL VARCHAR(40), " +
+        "NCHAR_COL NCHAR(40), NVARCHAR_COL NVARCHAR(40), VARBINARY_COL BINARY VARYING(10), " +
+        "ST_GEOMETRY_COL ST_GEOMETRY(10), DATE_COL DATE, TIME_COL TIME, TIMETZ_COL TIMETZ, TIMESTAMP_COL TIMESTAMP, " +
+        "INTERVAL_COL INTERVAL, BOOLEAN_COL BOOLEAN)");
+
+      stmt.execute("CREATE TABLE MY_DEST_TABLE AS SELECT * FROM my_table");
+      stmt.execute("CREATE TABLE YOUR_TABLE AS SELECT * FROM my_table");
     }
   }
 
   protected static void prepareTestData(Connection conn) throws SQLException {
-    try (
-      Statement stmt = conn.createStatement();
-      PreparedStatement pStmt1 =
-        conn.prepareStatement("INSERT INTO my_table " +
-                                "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?," +
-                                "       ?, ?, ?, ?, ?, ?, ?, ?, ?, ?," +
-                                "       ?, ?, ?, ?, ?)");
-      PreparedStatement pStmt2 =
-        conn.prepareStatement("INSERT INTO your_table " +
-                                "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?," +
-                                "       ?, ?, ?, ?, ?, ?, ?, ?, ?, ?," +
-                                "       ?, ?, ?, ?, ?)")) {
-
+    try (Statement stmt = conn.createStatement()) {
       stmt.execute("insert into db_action_test values (1, '1970-01-01')");
       stmt.execute("insert into post_action_test values (1, '1970-01-01')");
 
-      populateData(pStmt1, pStmt2);
-    }
-  }
+      for (String tableName : Arrays.asList("MY_TABLE", "YOUR_TABLE")) {
+        for (int i = 1; i <= 5; i++) {
+          String name = "user" + i;
+          try (PreparedStatement pStmt = conn.prepareStatement("insert into " + tableName + " values(" +
+            "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+            pStmt.setString(11, name);
+            pStmt.setString(12, name);
+            pStmt.setString(13, name);
+            pStmt.setString(14, name);
+            pStmt.setBytes(15, name.getBytes(Charsets.UTF_8));
+            pStmt.setBytes(16, name.getBytes(Charsets.UTF_8));
 
-  private static void populateData(PreparedStatement ...stmts) throws SQLException {
-    // insert the same data into both tables: my_table and your_table
-    for (PreparedStatement pStmt : stmts) {
-      for (int i = 1; i <= 5; i++) {
-        String name = "user" + i;
-        pStmt.setInt(1, i);
-        pStmt.setString(2, name);
-        pStmt.setDouble(3, 123.45 + i);
-        pStmt.setBoolean(4, (i % 2 == 0));
-        pStmt.setString(5, "random" + i);
-        pStmt.setShort(6, (short) i);
-        pStmt.setShort(7, (short) i);
-        pStmt.setInt(8, i);
-        pStmt.setLong(9, (long) i);
-        pStmt.setString(10, name);
-        pStmt.setString(11, name);
-        pStmt.setString(12, name);
-        pStmt.setDate(13, new Date(CURRENT_TS));
-        pStmt.setTime(14, new Time(CURRENT_TS));
-        pStmt.setTime(15, new Time(CURRENT_TS));
-        pStmt.setTimestamp(16, new Timestamp(CURRENT_TS));
-        pStmt.setString(17, "2 year 3 month " + i + " day");
-        pStmt.setDouble(18, 123.45 + i);
-        pStmt.setBigDecimal(19, new BigDecimal(123.45).add(new BigDecimal(i)));
-        pStmt.setString(20, name);
-        pStmt.setDouble(21, 123.45 + i);
-        pStmt.setBytes(22, name.getBytes(Charsets.UTF_8));
-        pStmt.setBytes(23, name.getBytes(Charsets.UTF_8));
-        pStmt.setBigDecimal(24, new BigDecimal(123.45).add(new BigDecimal(i)));
-        pStmt.setFloat(25, (float) 123.45 + i);
+            pStmt.setInt(1, i);
+            pStmt.setInt(2, i);
+            pStmt.setInt(3, i);
+            pStmt.setLong(4, i);
 
-        pStmt.executeUpdate();
+            pStmt.setFloat(5, 123.45f + i);
+            pStmt.setFloat(6, 123.45f + i);
+            pStmt.setDouble(7, 123.45 + i);
+            pStmt.setDouble(8, 123.45 + i);
+            pStmt.setBigDecimal(
+              9,
+              new BigDecimal(123.45, MathContext.DECIMAL64).add(new BigDecimal(i, MathContext.DECIMAL64))
+            );
+            pStmt.setBigDecimal(
+              10,
+              new BigDecimal(123.45, MathContext.DECIMAL64).add(new BigDecimal(i, MathContext.DECIMAL64))
+            );
+            pStmt.setDate(17, new Date(CURRENT_TS));
+            pStmt.setTime(18, new Time(CURRENT_TS));
+            pStmt.setString(19, "13:24:16+03");
+            pStmt.setTimestamp(20, new Timestamp(CURRENT_TS));
+            pStmt.setString(21, "2 year 3 month " + i + " day");
+            // other
+            pStmt.setBoolean(22, (i % 2 == 0));
+
+            pStmt.execute();
+          }
+        }
       }
     }
   }
@@ -216,7 +194,7 @@ public class NetezzaPluginTestBase extends DatabasePluginTestBase {
     try {
       Class.forName(Driver.class.getCanonicalName());
       return DriverManager.getConnection(connectionUrl, BASE_PROPS.get(ConnectionConfig.USER),
-                                         BASE_PROPS.get(ConnectionConfig.PASSWORD));
+        BASE_PROPS.get(ConnectionConfig.PASSWORD));
     } catch (Exception e) {
       throw Throwables.propagate(e);
     }
--- netezza-plugin/src/test/java/io/cdap/plugin/netezza/NetezzaSinkTestRun.java
+++ netezza-plugin/src/test/java/io/cdap/plugin/netezza/NetezzaSinkTestRun.java
@@ -19,16 +19,21 @@ package io.cdap.plugin.netezza;
 import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+
+import io.cdap.cdap.api.common.Bytes;
 import io.cdap.cdap.api.data.format.StructuredRecord;
 import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.cdap.api.dataset.table.Table;
 import io.cdap.cdap.etl.api.batch.BatchSink;
 import io.cdap.cdap.etl.mock.batch.MockSource;
 import io.cdap.cdap.etl.proto.v2.ETLPlugin;
+import io.cdap.cdap.test.ApplicationManager;
 import io.cdap.cdap.test.DataSetManager;
 import io.cdap.plugin.common.Constants;
 import io.cdap.plugin.db.batch.sink.AbstractDBSink;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -51,12 +56,56 @@ import java.util.Set;
  * Test for ETL using databases.
  */
 public class NetezzaSinkTestRun extends NetezzaPluginTestBase {
+  private static final Schema SCHEMA = Schema.recordOf(
+    "dbRecord",
+    Schema.Field.of("INTEGER_COL", Schema.of(Schema.Type.INT)),
+    Schema.Field.of("BYTEINT_COL", Schema.of(Schema.Type.INT)),
+    Schema.Field.of("SMALLINT_COL", Schema.of(Schema.Type.INT)),
+    Schema.Field.of("BIGINT_COL", Schema.of(Schema.Type.LONG)),
+    Schema.Field.of("REAL_COL", Schema.of(Schema.Type.FLOAT)),
+    Schema.Field.of("REAL_FLOAT_COL", Schema.of(Schema.Type.FLOAT)),
+    Schema.Field.of("DOUBLE_FLOAT_COL", Schema.of(Schema.Type.DOUBLE)),
+    Schema.Field.of("DOUBLE_PRECISION_COL", Schema.of(Schema.Type.DOUBLE)),
+    Schema.Field.of("NUMERIC_COL", Schema.decimalOf(PRECISION, SCALE)),
+    Schema.Field.of("DECIMAL_COL", Schema.decimalOf(PRECISION, SCALE)),
+    Schema.Field.of("CHAR_COL", Schema.of(Schema.Type.STRING)),
+    Schema.Field.of("VARCHAR_COL", Schema.of(Schema.Type.STRING)),
+    Schema.Field.of("NCHAR_COL", Schema.of(Schema.Type.STRING)),
+    Schema.Field.of("NVARCHAR_COL", Schema.of(Schema.Type.STRING)),
+    Schema.Field.of("VARBINARY_COL", Schema.of(Schema.Type.BYTES)),
+    Schema.Field.of("ST_GEOMETRY_COL", Schema.of(Schema.Type.BYTES)),
+    Schema.Field.of("DATE_COL", Schema.of(Schema.LogicalType.DATE)),
+    Schema.Field.of("TIME_COL", Schema.of(Schema.LogicalType.TIME_MICROS)),
+    Schema.Field.of("TIMETZ_COL", Schema.of(Schema.Type.STRING)),
+    Schema.Field.of("TIMESTAMP_COL", Schema.of(Schema.LogicalType.TIMESTAMP_MICROS)),
+    Schema.Field.of("INTERVAL_COL", Schema.of(Schema.Type.STRING)),
+    Schema.Field.of("BOOLEAN_COL", Schema.of(Schema.Type.BOOLEAN))
+  );
+
+  private static final BigDecimal NUMERIC_VALUE = new BigDecimal(3.458d, new MathContext(PRECISION)).setScale(SCALE);
+
+  @Before
+  public void setup() throws Exception {
+    try (Statement stmt = createConnection().createStatement()) {
+      stmt.execute("DELETE FROM MY_DEST_TABLE");
+    }
+  }
 
   @Test
-  public void testDBSink() throws Exception {
-    String inputDatasetName = "input-dbsinktest";
+  public void testDBSinkWithExplicitInputSchema() throws Exception {
+    testDBSink("testDBSinkWithExplicitInputSchema", "input-dbsinktest-explicit", true);
+  }
 
-    ETLPlugin sourceConfig = MockSource.getPlugin(inputDatasetName);
+
+  @Test
+  public void testDBSinkWithInferredInputSchema() throws Exception {
+    testDBSink("testDBSinkWithInferredInputSchema", "input-dbsinktest-inferred", false);
+  }
+
+  public void testDBSink(String appName, String inputDatasetName, boolean setInputSchema) throws Exception {
+    ETLPlugin sourceConfig = (setInputSchema)
+      ? MockSource.getPlugin(inputDatasetName, SCHEMA)
+      : MockSource.getPlugin(inputDatasetName);
     ETLPlugin sinkConfig = new ETLPlugin(
       NetezzaConstants.PLUGIN_NAME,
       BatchSink.PLUGIN_TYPE,
@@ -67,77 +116,84 @@ public class NetezzaSinkTestRun extends NetezzaPluginTestBase {
         .build(),
       null);
 
-    deployETL(sourceConfig, sinkConfig, DATAPIPELINE_ARTIFACT, "testDBSink");
+    ApplicationManager appManager = deployETL(sourceConfig, sinkConfig, DATAPIPELINE_ARTIFACT, appName);
     createInputData(inputDatasetName);
 
+    runETLOnce(appManager, ImmutableMap.of("logical.start.time", String.valueOf(CURRENT_TS)));
 
     try (Connection conn = createConnection();
          Statement stmt = conn.createStatement();
-         ResultSet resultSet = stmt.executeQuery("SELECT * FROM my_table ORDER BY ID")) {
+         ResultSet resultSet = stmt.executeQuery("SELECT * FROM MY_DEST_TABLE ORDER BY INTEGER_COL")) {
+
       Set<String> users = new HashSet<>();
       Assert.assertTrue(resultSet.next());
-      users.add(resultSet.getString("NAME"));
+      users.add(resultSet.getString("VARCHAR_COL"));
+
+      Assert.assertEquals(1, resultSet.getInt("INTEGER_COL"));
+      Assert.assertEquals(1, resultSet.getInt("BYTEINT_COL"));
+      Assert.assertEquals(1, resultSet.getInt("SMALLINT_COL"));
+      Assert.assertEquals(1, resultSet.getLong("BIGINT_COL"));
+
+      Assert.assertEquals(3.451f + 1, resultSet.getFloat("REAL_COL"), 0.00001f);
+      Assert.assertEquals(3.451f + 1, resultSet.getFloat("REAL_FLOAT_COL"), 0.00001f);
+      Assert.assertEquals(3.451 + 1, resultSet.getFloat("DOUBLE_FLOAT_COL"), 0.000001);
+      Assert.assertEquals(3.451 + 1, resultSet.getFloat("DOUBLE_PRECISION_COL"), 0.000001);
+      Assert.assertEquals(NUMERIC_VALUE, resultSet.getBigDecimal("NUMERIC_COL"));
+
+      Assert.assertEquals("user1", resultSet.getString("CHAR_COL").trim());
+      Assert.assertEquals("user1", resultSet.getString("VARCHAR_COL"));
+      Assert.assertEquals("user1", resultSet.getString("NCHAR_COL").trim());
+      Assert.assertEquals("user1", resultSet.getString("NVARCHAR_COL"));
+      Assert.assertEquals("user1", Bytes.toString(resultSet.getBytes("VARBINARY_COL")));
+      Assert.assertEquals("user1", Bytes.toString(resultSet.getBytes("ST_GEOMETRY_COL")));
+
       Assert.assertEquals(new Date(CURRENT_TS).toString(), resultSet.getDate("DATE_COL").toString());
       Assert.assertEquals(new Time(CURRENT_TS).toString(), resultSet.getTime("TIME_COL").toString());
-      Assert.assertEquals(new Timestamp(CURRENT_TS),
-                          resultSet.getTimestamp("TIMESTAMP_COL"));
-      Assert.assertTrue(resultSet.next());
-      Assert.assertEquals(new BigDecimal(3.458, new MathContext(PRECISION)).setScale(SCALE),
-                          resultSet.getBigDecimal("NUMERIC_COL"));
-      Assert.assertEquals(new BigDecimal(3.459, new MathContext(PRECISION)).setScale(SCALE),
-                          resultSet.getBigDecimal("DECIMAL_COL"));
-      users.add(resultSet.getString("NAME"));
-      Assert.assertEquals(ImmutableSet.of("user1", "user2"), users);
+      Assert.assertEquals("13:24:16+03", resultSet.getString("TIMETZ_COL"));
+      Assert.assertEquals(new Timestamp(CURRENT_TS), resultSet.getTimestamp("TIMESTAMP_COL"));
+      Assert.assertEquals("2 years 3 mons 2 days", resultSet.getString("INTERVAL_COL"));
 
+      Assert.assertTrue(resultSet.getBoolean("BOOLEAN_COL"));
+
+      Assert.assertTrue(resultSet.next());
+      users.add(resultSet.getString("VARCHAR_COL"));
+
+      Assert.assertEquals(ImmutableSet.of("user1", "user2"), users);
     }
   }
 
   private void createInputData(String inputDatasetName) throws Exception {
     // add some data to the input table
     DataSetManager<Table> inputManager = getDataset(inputDatasetName);
-    Schema schema = Schema.recordOf(
-      "dbRecord",
-      Schema.Field.of("ID", Schema.of(Schema.Type.INT)),
-      Schema.Field.of("NAME", Schema.of(Schema.Type.STRING)),
-      Schema.Field.of("SCORE", Schema.of(Schema.Type.FLOAT)),
-      Schema.Field.of("GRADUATED", Schema.of(Schema.Type.BOOLEAN)),
-      Schema.Field.of("TINY", Schema.of(Schema.Type.INT)),
-      Schema.Field.of("SMALL", Schema.of(Schema.Type.INT)),
-      Schema.Field.of("BIG", Schema.of(Schema.Type.LONG)),
-      Schema.Field.of("FLOAT_COL", Schema.of(Schema.Type.FLOAT)),
-      Schema.Field.of("REAL_COL", Schema.of(Schema.Type.FLOAT)),
-      Schema.Field.of("NUMERIC_COL", Schema.decimalOf(PRECISION, SCALE)),
-      Schema.Field.of("DECIMAL_COL", Schema.decimalOf(PRECISION, SCALE)),
-      Schema.Field.of("BIT_COL", Schema.of(Schema.Type.BOOLEAN)),
-      Schema.Field.of("DATE_COL", Schema.of(Schema.LogicalType.DATE)),
-      Schema.Field.of("TIME_COL", Schema.of(Schema.LogicalType.TIME_MICROS)),
-      Schema.Field.of("TIMESTAMP_COL", Schema.of(Schema.LogicalType.TIMESTAMP_MICROS)),
-      Schema.Field.of("BINARY_COL", Schema.of(Schema.Type.BYTES)),
-      Schema.Field.of("BLOB_COL", Schema.of(Schema.Type.BYTES))
-    );
+
     List<StructuredRecord> inputRecords = new ArrayList<>();
     LocalDateTime localDateTime = new Timestamp(CURRENT_TS).toLocalDateTime();
     for (int i = 1; i <= 2; i++) {
       String name = "user" + i;
-      inputRecords.add(StructuredRecord.builder(schema)
-                         .set("ID", i)
-                         .set("NAME", name)
-                         .set("SCORE", 3.451f)
-                         .set("GRADUATED", (i % 2 == 0))
-                         .set("TINY", i + 1)
-                         .set("SMALL", i + 2)
-                         .set("BIG", 3456987L)
-                         .set("FLOAT_COL", 3.456f)
-                         .set("REAL_COL", 3.457f)
-                         .setDecimal("NUMERIC_COL", new BigDecimal(3.458d, new MathContext(PRECISION)).setScale(SCALE))
-                         .setDecimal("DECIMAL_COL", new BigDecimal(3.459d, new MathContext(PRECISION)).setScale(SCALE))
-                         .set("BIT_COL", (i % 2 == 1))
-                         .setDate("DATE_COL", localDateTime.toLocalDate())
-                         .setTime("TIME_COL", localDateTime.toLocalTime())
-                         .setTimestamp("TIMESTAMP_COL", localDateTime.atZone(ZoneId.ofOffset("UTC", ZoneOffset.UTC)))
-                         .set("BINARY_COL", name.getBytes(Charsets.UTF_8))
-                         .set("BLOB_COL", name.getBytes(Charsets.UTF_8))
-                         .build());
+      inputRecords.add(StructuredRecord.builder(SCHEMA)
+        .set("INTEGER_COL", i)
+        .set("BYTEINT_COL", i)
+        .set("SMALLINT_COL", i)
+        .set("BIGINT_COL", (long) i)
+        .set("REAL_COL", 3.451f + i)
+        .set("REAL_FLOAT_COL", 3.451f + i)
+        .set("DOUBLE_FLOAT_COL", 3.451 + i)
+        .set("DOUBLE_PRECISION_COL", 3.451 + i)
+        .setDecimal("NUMERIC_COL", NUMERIC_VALUE)
+        .setDecimal("DECIMAL_COL", NUMERIC_VALUE)
+        .set("CHAR_COL", name)
+        .set("VARCHAR_COL", name)
+        .set("NCHAR_COL", name)
+        .set("NVARCHAR_COL", name)
+        .set("VARBINARY_COL", name.getBytes(Charsets.UTF_8))
+        .set("ST_GEOMETRY_COL", name.getBytes(Charsets.UTF_8))
+        .setDate("DATE_COL", localDateTime.toLocalDate())
+        .setTime("TIME_COL", localDateTime.toLocalTime())
+        .set("TIMETZ_COL", "13:24:16+03")
+        .setTimestamp("TIMESTAMP_COL", localDateTime.atZone(ZoneId.ofOffset("UTC", ZoneOffset.UTC)))
+        .set("INTERVAL_COL", "2 years 3 mons 2 days")
+        .set("BOOLEAN_COL", true)
+        .build());
     }
     MockSource.writeInput(inputManager, inputRecords);
   }
