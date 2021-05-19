33c053f32a5bf07c144340f23c0c97ab090c8861
Mo Eseifan <meseifan@google.com>
2021-05-19 12:36:38 -0700

    Fixed NPE when working with nullable time field in MSSQL

--- mssql-plugin/src/main/java/io/cdap/plugin/mssql/SqlServerSourceDBRecord.java
+++ mssql-plugin/src/main/java/io/cdap/plugin/mssql/SqlServerSourceDBRecord.java
@@ -21,15 +21,15 @@ import io.cdap.cdap.api.data.schema.Schema;
 import io.cdap.plugin.db.ColumnType;
 import io.cdap.plugin.db.DBRecord;
 import io.cdap.plugin.db.SchemaReader;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.List;
 
 /**
- * SQL Server Source implementation {@link org.apache.hadoop.mapreduce.lib.db.DBWritable} and
- * {@link org.apache.hadoop.io.Writable}.
+ * SQL Server Source implementation {@link org.apache.hadoop.mapreduce.lib.db.DBWritable} and {@link
+ * org.apache.hadoop.io.Writable}.
  */
 public class SqlServerSourceDBRecord extends DBRecord {
 
@@ -42,14 +42,17 @@ public class SqlServerSourceDBRecord extends DBRecord {
   }
 
   @Override
-  protected void handleField(ResultSet resultSet, StructuredRecord.Builder recordBuilder, Schema.Field field,
-                             int columnIndex, int sqlType, int sqlPrecision, int sqlScale) throws SQLException {
+  protected void handleField(ResultSet resultSet, StructuredRecord.Builder recordBuilder,
+      Schema.Field field,
+      int columnIndex, int sqlType, int sqlPrecision, int sqlScale) throws SQLException {
     switch (sqlType) {
       case Types.TIME:
         // Handle reading SQL Server 'TIME' data type to avoid accuracy loss.
         // 'TIME' data type has the accuracy of 100 nanoseconds(1 millisecond in Informatica)
         // but reading via 'getTime' and 'getObject' will round value to second.
-        recordBuilder.setTime(field.getName(), resultSet.getTimestamp(columnIndex).toLocalDateTime().toLocalTime());
+        final Timestamp timestamp = resultSet.getTimestamp(columnIndex);
+        recordBuilder.setTime(field.getName(),
+            timestamp == null ? null : timestamp.toLocalDateTime().toLocalTime());
         break;
       case SqlServerSourceSchemaReader.DATETIME_OFFSET_TYPE:
         recordBuilder.set(field.getName(), resultSet.getString(columnIndex));
