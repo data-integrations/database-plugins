5ada9e74f48a6b52bf2dee75578a61685f7d2e06
ellliao <elliao@google.com>
2022-08-10 20:50:52 +0000

    Add unit tests for OracleConnector.java

---
+++ oracle-plugin/src/test/java/io/cdap/plugin/oracle/OracleConnectorUnitTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.plugin.oracle;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class OracleConnectorUnitTest {
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    private static final OracleConnector CONNECTOR = new OracleConnector(null);
+
+    /**
+     * Unit test for getTableName()
+     */
+    @Test
+    public void getTableNameTest() {
+      Assert.assertEquals("\"schema\".\"table\"",
+                          CONNECTOR.getTableName("db", "schema", "table"));
+    }
+
+  /**
+   * Unit tests for getTableQuery()
+   */
+  @Test
+  public void getTableQueryTest() {
+    String tableName = CONNECTOR.getTableName("db", "schema", "table");
+
+    // default query, sampleType "first"
+    Assert.assertEquals(String.format("SELECT * FROM %s WHERE ROWNUM <= %d", tableName, 100),
+                        CONNECTOR.getTableQuery("db", "schema", "table",
+                                                100, "first", null));
+    // default query, sampleType null
+    Assert.assertEquals(String.format("SELECT * FROM %s WHERE ROWNUM <= %d", tableName, 100),
+                        CONNECTOR.getTableQuery("db", "schema", "table",
+                                                100, null, null));
+    // random query
+    Assert.assertEquals(String.format("SELECT * FROM (\n" +
+                                        "SELECT * FROM %s ORDER BY DBMS_RANDOM.RANDOM\n" +
+                                        ")\n" +
+                                        "WHERE rownum <= %d",
+                                      tableName, 100),
+                        CONNECTOR.getTableQuery("db", "schema", "table",
+                                                100, "random", null));
+    // stratified query
+    Assert.assertEquals(String.format("SELECT * FROM (\n" +
+                                        "SELECT * FROM %s ORDER BY DBMS_RANDOM.RANDOM\n" +
+                                        ")\n" +
+                                        "WHERE rownum <= %d\n" +
+                                        "ORDER BY %s",
+                                      tableName, 100, "strata"),
+                        CONNECTOR.getTableQuery("db", "schema", "table",
+                                                100, "stratified", "strata"));
+  }
+
+  /**
+   * Test for null strata exception
+   *
+   * @throws IllegalArgumentException
+   */
+  @Test
+  public void getTableQueryNullStrataTest() throws IllegalArgumentException {
+    expectedEx.expect(IllegalArgumentException.class);
+    CONNECTOR.getTableQuery("db", "schema", "table", 100, "stratified", null);
+  }
+}
