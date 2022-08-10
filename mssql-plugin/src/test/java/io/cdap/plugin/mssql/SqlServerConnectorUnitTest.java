77cd5548a657a7d7774eeb17a1dadbebc0b3872d
ellliao <elliao@google.com>
2022-08-10 21:10:53 +0000

    Add unit tests for SqlServerConnector.java

---
+++ mssql-plugin/src/test/java/io/cdap/plugin/mssql/SqlServerConnectorUnitTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright © 2022 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.plugin.mssql;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Unit tests for {@link SqlServerConnector}
+ */
+public class SqlServerConnectorUnitTest {
+  @Rule
+  public ExpectedException expectedEx = ExpectedException.none();
+
+  private static final SqlServerConnector CONNECTOR = new SqlServerConnector(null);
+
+  /**
+   * Unit tests for getTableQuery()
+   */
+  @Test
+  public void getTableQueryTest() {
+    String tableName = "\"db\".\"schema\".\"table\"";
+
+    // default query, sampleType "first"
+    Assert.assertEquals(String.format("SELECT TOP %d * FROM %s", 100, tableName),
+                        CONNECTOR.getTableQuery("db", "schema", "table",
+                                                100, "first", null));
+    // default query, sampleType null
+    Assert.assertEquals(String.format("SELECT TOP %d * FROM %s", 100, tableName),
+                        CONNECTOR.getTableQuery("db", "schema", "table",
+                                                100, null, null));
+    // random query
+    Assert.assertEquals(String.format("SELECT * FROM %s " +
+                                        "WHERE (ABS(CAST((BINARY_CHECKSUM(*) * RAND()) as int)) %% 100) " +
+                                        "< %d / (SELECT COUNT(*) FROM %s)",
+                                      tableName, 10000, tableName),
+                        CONNECTOR.getTableQuery("db", "schema", "table",
+                                                100, "random", null));
+    // stratified query
+    Assert.assertEquals(String.format("SELECT * FROM %s " +
+                                        "WHERE (ABS(CAST((BINARY_CHECKSUM(*) * RAND()) as int)) %% 100) " +
+                                        "< %d / (SELECT COUNT(*) FROM %s)" +
+                                        "ORDER BY %s",
+                                      tableName, 10000, tableName, "strata"),
+                        CONNECTOR.getTableQuery("db", "schema", "table",
+                                                100, "stratified", "strata"));
+  }
+
+  /**
+   * Test for null strata exception
+   *
+   * @throws IllegalArgumentException
+   */
+  @Test
+  public void getTableQueryNullStrataTest() throws IllegalArgumentException {
+    expectedEx.expect(IllegalArgumentException.class);
+    CONNECTOR.getTableQuery("db", "schema", "table", 100, "stratified", null);
+  }
+}
