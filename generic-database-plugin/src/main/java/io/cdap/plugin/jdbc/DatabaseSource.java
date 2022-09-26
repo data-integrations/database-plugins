4d9fa2f6769c5b0894bfd637313878732fd338f5
Apoorva M K <apoorvamk@google.com>
2022-09-26 14:50:35 +0000

    Some other changes

--- generic-database-plugin/src/main/java/io/cdap/plugin/jdbc/DatabaseSource.java
+++ generic-database-plugin/src/main/java/io/cdap/plugin/jdbc/DatabaseSource.java
@@ -16,10 +16,15 @@
 
 package io.cdap.plugin.jdbc;
 
+import com.google.common.base.Strings;
 import io.cdap.cdap.api.annotation.Description;
 import io.cdap.cdap.api.annotation.Macro;
 import io.cdap.cdap.api.annotation.Name;
 import io.cdap.cdap.api.annotation.Plugin;
+import io.cdap.cdap.etl.api.batch.BatchSourceContext;
+import io.cdap.plugin.common.Asset;
+import io.cdap.plugin.common.LineageRecorder;
+import io.cdap.plugin.common.ReferenceNames;
 import io.cdap.plugin.db.ConnectionConfig;
 import io.cdap.plugin.db.batch.source.AbstractDBSource;
 
@@ -55,11 +60,6 @@ public class DatabaseSource extends AbstractDBSource<DatabaseSource.DatabaseSour
     @Macro
     public String connectionString;
 
-    @Name(REFERENCE_NAME)
-    @Description("FQN for Database Sink")
-    @Macro
-    public String referenceName;
-
     @Nullable
     @Name(TRANSACTION_ISOLATION_LEVEL)
     @Description("The transaction isolation level for queries run by this sink. " +
@@ -75,9 +75,12 @@ public class DatabaseSource extends AbstractDBSource<DatabaseSource.DatabaseSour
     }
 
     @Override
-    public String getReferenceName() {
-      referenceName = DatabaseURLParser.getFQN(connectionString);
-      return referenceName;
+    protected LineageRecorder getLineageRecorder(BatchSourceContext context) {
+      // dbtype, host, port, db from the connection string
+      // table is the reference name
+      String fqn = "oracle://{host}:{port}/{database}.{table}";
+      Asset asset = Asset.builder(databaseSourceConfig.getReferenceName()).setFqn(fqn).build();
+      return new LineageRecorder(context, asset);
     }
   }
 }
