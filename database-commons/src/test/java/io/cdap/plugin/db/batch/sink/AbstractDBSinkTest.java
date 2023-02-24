2f5803e19b1374d0c61d70d9b50589f6f92a84e7
Rahul Sharma <112750762+MrRahulSharma@users.noreply.github.com>
2023-02-24 12:42:25 +0530

    Fixing the ResultSet look up logic to look against the column name instead of field index from Schema. (#356)
    Removed the buggy logic of index in Oracle plugin

--- database-commons/src/test/java/io/cdap/plugin/db/batch/sink/AbstractDBSinkTest.java
+++ database-commons/src/test/java/io/cdap/plugin/db/batch/sink/AbstractDBSinkTest.java
@@ -17,6 +17,7 @@
 package io.cdap.plugin.db.batch.sink;
 
 import com.google.common.collect.ImmutableList;
+import com.mockrunner.mock.jdbc.MockResultSet;
 import com.mockrunner.mock.jdbc.MockResultSetMetaData;
 import io.cdap.plugin.db.ColumnType;
 import org.junit.Assert;
@@ -24,7 +25,9 @@ import org.junit.Test;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Test class for abstract sink.
@@ -32,7 +35,7 @@ import java.util.List;
 public class AbstractDBSinkTest {
 
   @Test
-  public void testGetMatchedColumnTypeList() throws SQLException {
+  public void testGetMatchedColumnTypeList() throws Exception {
     List<String> columns = ImmutableList.of(
       "ID",
       "NAME",
@@ -45,20 +48,25 @@ public class AbstractDBSinkTest {
     resultSetMetaData.setColumnCount(columns.size());
 
     for (int i = 0; i < columns.size(); i++) {
-      String name = columns.get(i);
       resultSetMetaData.setColumnName(i + 1, columns.get(i));
       resultSetMetaData.setColumnTypeName(i + 1, "STRING");
       resultSetMetaData.setColumnType(i + 1, i);
-      expectedColumns.add(new ColumnType(name, "STRING", i));
+      expectedColumns.add(new ColumnType(columns.get(i), "STRING", i));
     }
 
-    List<ColumnType> result = AbstractDBSink.getMatchedColumnTypeList(resultSetMetaData, columns);
+    MockResultSet resultSet = new MockResultSet("data");
+    Set<String> columnNamesSet = new HashSet<String>();
+    columnNamesSet.addAll(columns);
+    resultSet.addColumns(columnNamesSet);
+    resultSet.setResultSetMetaData(resultSetMetaData);
+
+    List<ColumnType> result = AbstractDBSink.getMatchedColumnTypeList(resultSet, columns);
 
     Assert.assertEquals(expectedColumns, result);
   }
 
   @Test
-  public void testGetMismatchColumnTypeList() throws SQLException {
+  public void testGetMismatchColumnTypeList() throws Exception {
     List<String> wrongColumns = ImmutableList.of(
       "MY_ID",
       "NAME",
@@ -80,12 +88,119 @@ public class AbstractDBSinkTest {
       resultSetMetaData.setColumnType(i + 1, i);
     }
 
+    MockResultSet resultSet = new MockResultSet("data");
+    Set<String> columnNamesSet = new HashSet<String>();
+    columnNamesSet.addAll(columns);
+    resultSet.addColumns(columnNamesSet);
+    resultSet.setResultSetMetaData(resultSetMetaData);
+
     try {
-      AbstractDBSink.getMatchedColumnTypeList(resultSetMetaData, wrongColumns);
+      AbstractDBSink.getMatchedColumnTypeList(resultSet, wrongColumns);
       Assert.fail(String.format("Expected to throw %s", IllegalArgumentException.class.getName()));
     } catch (IllegalArgumentException e) {
       String errorMessage = "Missing column 'MY_ID' in SQL table";
       Assert.assertEquals(errorMessage, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testDifferentOrderOfFieldsInResultSet() throws Exception {
+    List<String> diffOrdCol = ImmutableList.of(
+      "Name",
+      "SCORE",
+      "ID"
+    );
+
+    List<String> columns = ImmutableList.of(
+      "ID",
+      "NAME",
+      "SCORE"
+    );
+
+    List<String> typeName = ImmutableList.of(
+      "INT",
+      "STRING",
+      "DOUBLE"
+    );
+
+    List<Integer> typeValue = ImmutableList.of(
+      1,
+      2,
+      3
+    );
+
+    List<ColumnType> expectedColumns = new ArrayList<>();
+    MockResultSetMetaData resultSetMetaData = new MockResultSetMetaData();
+    resultSetMetaData.setColumnCount(columns.size());
+
+    for (int i = 0; i < columns.size(); i++) {
+      resultSetMetaData.setColumnName(i + 1, columns.get(i));
+      resultSetMetaData.setColumnTypeName(i + 1, typeName.get(i));
+      resultSetMetaData.setColumnType(i + 1, typeValue.get(i));
+      expectedColumns.add(new ColumnType(columns.get(i), typeName.get(i), typeValue.get(i)));
+    }
+
+    MockResultSet resultSet = new MockResultSet("data");
+    Set<String> columnNamesSet = new HashSet<String>();
+    columnNamesSet.addAll(columns);
+    resultSet.addColumns(columnNamesSet);
+    resultSet.setResultSetMetaData(resultSetMetaData);
+
+    List<ColumnType> actualColumns = AbstractDBSink.getMatchedColumnTypeList(resultSet, diffOrdCol);
+
+    // Assert that all expected fields are present in the actual fields
+    for (ColumnType exColType : expectedColumns) {
+      Assert.assertTrue(actualColumns.contains(exColType));
+    }
+  }
+
+  @Test
+  public void testSubsetColumnsInResultSet() throws Exception {
+    List<String> subsetCol = ImmutableList.of(
+      "SCORE",
+      "ID"
+    );
+
+    List<String> columns = ImmutableList.of(
+      "ID",
+      "NAME",
+      "SCORE"
+    );
+
+    List<String> typeName = ImmutableList.of(
+      "INT",
+      "STRING",
+      "DOUBLE"
+    );
+
+    List<Integer> typeValue = ImmutableList.of(
+      1,
+      2,
+      3
+    );
+
+    List<ColumnType> expectedColumns = new ArrayList<>();
+    MockResultSetMetaData resultSetMetaData = new MockResultSetMetaData();
+    resultSetMetaData.setColumnCount(columns.size());
+
+    for (int i = 0; i < columns.size(); i++) {
+      resultSetMetaData.setColumnName(i + 1, columns.get(i));
+      resultSetMetaData.setColumnTypeName(i + 1, typeName.get(i));
+      resultSetMetaData.setColumnType(i + 1, typeValue.get(i));
+      expectedColumns.add(new ColumnType(columns.get(i), typeName.get(i), typeValue.get(i)));
+    }
+
+    MockResultSet resultSet = new MockResultSet("data");
+    Set<String> columnNamesSet = new HashSet<String>();
+    columnNamesSet.addAll(columns);
+    resultSet.addColumns(columnNamesSet);
+    resultSet.setResultSetMetaData(resultSetMetaData);
+
+    List<ColumnType> actualColumns = AbstractDBSink.getMatchedColumnTypeList(resultSet, subsetCol);
+
+    // Assert that all actual fields are present in the expected fields
+    for (ColumnType acColType : actualColumns) {
+      Assert.assertTrue(expectedColumns.contains(acColType));
     }
   }
 }
