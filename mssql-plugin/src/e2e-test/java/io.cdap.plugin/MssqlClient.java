335ba95e17ba5311fd472e1e268b25685c6a4cdc
Nikitapaliwal123 <102667534+Nikitapaliwal123@users.noreply.github.com>
2023-03-14 16:26:59 +0530

    Created basic infra of e2e tests for SQL Server plugin (#363)
    * Created basic infra of e2e tests for SQL Server plugin
    
    * Created basic infra of e2e tests for SQL Server plugin
    
    * Created basic infra of e2e tests for SQL Server plugin
    
    * Created basic infra of e2e tests for SQL Server plugin
    
    * Created basic infra of e2e tests for SQL Server plugin
    
    * Basic e2e infra for mssql
    
    * e2e infra

---
+++ mssql-plugin/src/e2e-test/java/io.cdap.plugin/MssqlClient.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright © 2023 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.plugin;
+
+import io.cdap.e2e.utils.PluginPropertyUtils;
+import org.junit.Assert;
+
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
+/**
+ *  Mssql client.
+ */
+public class MssqlClient {
+
+    private static Connection getMssqlConnection() throws SQLException, ClassNotFoundException {
+        TimeZone timezone = TimeZone.getTimeZone("UTC");
+        TimeZone.setDefault(timezone);
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String databaseName = PluginPropertyUtils.pluginProp("databaseName");
+        return DriverManager.getConnection("jdbc:sqlserver://" + System.getenv("MSSQL_HOST")
+                        + ":" + System.getenv("MSSQL_PORT") + ";databaseName=" + databaseName,
+                System.getenv("MSSQL_USERNAME"), System.getenv("MSSQL_PASSWORD"));
+    }
+
+    public static int countRecord(String table, String schema) throws SQLException, ClassNotFoundException {
+        String countQuery = "SELECT COUNT(*) as total FROM " + schema + "." + table;
+        try (Connection connect = getMssqlConnection(); Statement statement = connect.createStatement();
+             ResultSet rs = statement.executeQuery(countQuery)) {
+            int num = 0;
+            while (rs.next()) {
+                num = (rs.getInt(1));
+            }
+            return num;
+        }
+    }
+
+    public static void createSourceTable(String sourceTable, String schema) throws SQLException,
+            ClassNotFoundException {
+        try (Connection connect = getMssqlConnection(); Statement statement = connect.createStatement()) {
+            String createSourceTableQuery = "CREATE TABLE " + schema + "." + sourceTable +
+                    "(ID varchar(100), LASTNAME varchar(100))";
+            statement.executeUpdate(createSourceTableQuery);
+
+            // Insert dummy data.
+            statement.executeUpdate("INSERT INTO " + schema + "." + sourceTable + " (ID, LASTNAME)" +
+                    " VALUES ('id1', 'Shelby')");
+            statement.executeUpdate("INSERT INTO " + schema + "." + sourceTable + " (ID, LASTNAME)" +
+                    " VALUES ('id2', 'Simpson')");
+        }
+    }
+
+    public static void createTargetTable(String targetTable, String schema) throws SQLException,
+            ClassNotFoundException {
+        try (Connection connect = getMssqlConnection(); Statement statement = connect.createStatement()) {
+            String createTargetTableQuery = "CREATE TABLE " + schema + "." + targetTable +
+                    "(ID varchar(100), LASTNAME varchar(100))";
+            statement.executeUpdate(createTargetTableQuery);
+        }
+    }
+
+    public static void deleteTables(String schema, String[] tables)
+            throws SQLException, ClassNotFoundException {
+        try (Connection connect = getMssqlConnection(); Statement statement = connect.createStatement()) {
+            for (String table : tables) {
+                String dropTableQuery = "DROP TABLE " + schema + "." + table;
+                statement.execute(dropTableQuery);
+            }
+        }
+    }
+
+    public static boolean validateRecordValues(String schema, String sourceTable, String targetTable)
+            throws SQLException, ClassNotFoundException {
+        String getSourceQuery = "SELECT * FROM " + schema + "." + sourceTable;
+        String getTargetQuery = "SELECT * FROM " + schema + "." + targetTable;
+        try (Connection connect = getMssqlConnection()) {
+            connect.setHoldability(ResultSet.HOLD_CURSORS_OVER_COMMIT);
+            Statement statement1 = connect.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE,
+                    ResultSet.HOLD_CURSORS_OVER_COMMIT);
+            Statement statement2 = connect.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE,
+                    ResultSet.HOLD_CURSORS_OVER_COMMIT);
+            ResultSet rsSource = statement1.executeQuery(getSourceQuery);
+            ResultSet rsTarget = statement2.executeQuery(getTargetQuery);
+            return compareResultSetData(rsSource, rsTarget);
+        }
+    }
+
+    private static boolean compareResultSetData(ResultSet rsSource, ResultSet rsTarget) throws SQLException {
+        ResultSetMetaData mdSource = rsSource.getMetaData();
+        ResultSetMetaData mdTarget = rsTarget.getMetaData();
+        int columnCountSource = mdSource.getColumnCount();
+        int columnCountTarget = mdTarget.getColumnCount();
+        Assert.assertEquals("Number of columns in source and target are not equal",
+                columnCountSource, columnCountTarget);
+        while (rsSource.next() && rsTarget.next()) {
+            int currentColumnCount = 1;
+            while (currentColumnCount <= columnCountSource) {
+                String columnTypeName = mdSource.getColumnTypeName(currentColumnCount);
+                int columnType = mdSource.getColumnType(currentColumnCount);
+                String columnName = mdSource.getColumnName(currentColumnCount);
+                switch (columnType) {
+
+                    default:
+                        String sourceString = rsSource.getString(currentColumnCount);
+                        String targetString = rsTarget.getString(currentColumnCount);
+                        Assert.assertTrue(String.format("Different values found for column : %s", columnName),
+                                sourceString.equals(targetString));
+                }
+                currentColumnCount++;
+            }
+        }
+        Assert.assertFalse("Number of rows in Source table is greater than the number of rows in Target table",
+                rsSource.next());
+        Assert.assertFalse("Number of rows in Target table is greater than the number of rows in Source table",
+                rsTarget.next());
+        return true;
+    }
+}
---
+++ mssql-plugin/src/e2e-test/java/io.cdap.plugin/mssql/stepsdesign/Mssql.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2023 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.plugin.mssql.stepsdesign;
+
+import io.cdap.e2e.utils.CdfHelper;
+import io.cdap.e2e.utils.PluginPropertyUtils;
+import io.cdap.plugin.MssqlClient;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import stepsdesign.BeforeActions;
+
+import java.sql.SQLException;
+
+/**
+ *  Mssql Plugin related step design.
+ */
+public class Mssql implements CdfHelper {
+
+    @Then("Click on preview data for Mssql sink")
+    public void clickOnPreviewDataForMssqlSink() {
+        openSinkPluginPreviewData("SqlServer");
+    }
+
+    @Then("Validate records transferred to target table are equal to number of records from the source table")
+    public void validateRecordsTransferredToTargetTableAreEqualToTheNumberOfRecordsTheFromSourceTable() throws
+            SQLException, ClassNotFoundException {
+        int countRecords = MssqlClient.countRecord(PluginPropertyUtils.pluginProp("targetTable"),
+                PluginPropertyUtils.pluginProp("schema"));
+        Assert.assertEquals("Number of records transferred should be equal to records out ",
+                countRecords, recordOut());
+        BeforeActions.scenario.write("******** Number of records transferred ********* : " + countRecords);
+        boolean recordsMatched = MssqlClient.validateRecordValues(PluginPropertyUtils.pluginProp("schema"),
+                PluginPropertyUtils.pluginProp("sourceTable"),
+                PluginPropertyUtils.pluginProp("targetTable"));
+        Assert.assertTrue("Value of records transferred to the target table should be equal to the value " +
+                "of the records in the source table", recordsMatched);
+    }
+
+}
