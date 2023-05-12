2de8d65d321775be5942e3f88d0e38361d2e89ce
priyabhatnagar <priya.bhatnagar@cloudsufi.com>
2023-05-12 16:13:28 +0530

    SQL Server e2e

--- mssql-plugin/src/e2e-test/java/io.cdap.plugin/common.stepsdesign/TestSetupHooks.java
+++ mssql-plugin/src/e2e-test/java/io.cdap.plugin/common.stepsdesign/TestSetupHooks.java
@@ -16,15 +16,24 @@
 
 package io.cdap.plugin.common.stepsdesign;
 
+import com.google.cloud.bigquery.BigQueryException;
+import io.cdap.e2e.utils.BigQueryClient;
 import io.cdap.e2e.utils.PluginPropertyUtils;
 import io.cdap.plugin.MssqlClient;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 import stepsdesign.BeforeActions;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 /**
  * Mssql test hooks.
@@ -90,4 +99,77 @@ public class TestSetupHooks {
         BeforeActions.scenario.write("BigQuery Target table name: " + bqTargetTable);
     }
 
+    @After(order = 1, value = "@BQ_SINK_CLEANUP")
+    public static void deleteTempTargetBQTable() throws IOException, InterruptedException {
+        try {
+            BigQueryClient.dropBqQuery(bqTargetTable);
+            BeforeActions.scenario.write("BigQuery Target table: " + bqTargetTable + " is deleted successfully");
+            bqTargetTable = StringUtils.EMPTY;
+        } catch (BigQueryException e) {
+            if (e.getCode() == 404) {
+                BeforeActions.scenario.write("BigQuery Target Table: " + bqTargetTable + " does not exist");
+            } else {
+                Assert.fail(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Create BigQuery table.
+     */
+    @Before(order = 1, value = "@BQ_SOURCE_TEST")
+    public static void createTempSourceBQTable() throws IOException, InterruptedException {
+        createSourceBQTableWithQueries(PluginPropertyUtils.pluginProp("CreateBQTableQueryFile"),
+                PluginPropertyUtils.pluginProp("InsertBQDataQueryFile"));
+    }
+
+    @After(order = 1, value = "@BQ_SOURCE_TEST")
+    public static void deleteTempSourceBQTable() throws IOException, InterruptedException {
+        String bqSourceTable = PluginPropertyUtils.pluginProp("bqSourceTable");
+        BigQueryClient.dropBqQuery(bqSourceTable);
+        BeforeActions.scenario.write("BQ source Table " + bqSourceTable + " deleted successfully");
+        PluginPropertyUtils.removePluginProp("bqSourceTable");
+    }
+
+    private static void createSourceBQTableWithQueries(String bqCreateTableQueryFile, String bqInsertDataQueryFile)
+            throws IOException, InterruptedException {
+        String bqSourceTable = "E2E_SOURCE_" + UUID.randomUUID().toString().substring(0, 5).replaceAll("-",
+                "_");
+        System.out.println(bqSourceTable);
+
+        String createTableQuery = StringUtils.EMPTY;
+        try {
+            createTableQuery = new String(Files.readAllBytes(Paths.get(TestSetupHooks.class.getResource
+                    ("/" + bqCreateTableQueryFile).toURI()))
+                    , StandardCharsets.UTF_8);
+            createTableQuery = createTableQuery.replace("DATASET", PluginPropertyUtils.pluginProp("dataset"))
+                    .replace("TABLE_NAME", bqSourceTable);
+        } catch (Exception e) {
+            BeforeActions.scenario.write("Exception in reading " + bqCreateTableQueryFile + " - " + e.getMessage());
+            Assert.fail("Exception in BigQuery testdata prerequisite setup " +
+                    "- error in reading create table query file " + e.getMessage());
+        }
+
+        String insertDataQuery = StringUtils.EMPTY;
+        try {
+            insertDataQuery = new String(Files.readAllBytes(Paths.get(TestSetupHooks.class.getResource
+                    ("/" + bqInsertDataQueryFile).toURI()))
+                    , StandardCharsets.UTF_8);
+            insertDataQuery = insertDataQuery.replace("DATASET", PluginPropertyUtils.pluginProp("dataset"))
+                    .replace("TABLE_NAME", bqSourceTable);
+        } catch (Exception e) {
+            BeforeActions.scenario.write("Exception in reading " + bqInsertDataQueryFile + " - " + e.getMessage());
+            Assert.fail("Exception in BigQuery testdata prerequisite setup " +
+                    "- error in reading insert data query file " + e.getMessage());
+        }
+        BigQueryClient.getSoleQueryResult(createTableQuery);
+        try {
+            BigQueryClient.getSoleQueryResult(insertDataQuery);
+        } catch (NoSuchElementException e) {
+            // Insert query does not return any record.
+            // Iterator on TableResult values in getSoleQueryResult method throws NoSuchElementException
+        }
+        PluginPropertyUtils.addPluginProp("bqSourceTable", bqSourceTable);
+        BeforeActions.scenario.write("BQ Source Table " + bqSourceTable + " created successfully");
+    }
 }
