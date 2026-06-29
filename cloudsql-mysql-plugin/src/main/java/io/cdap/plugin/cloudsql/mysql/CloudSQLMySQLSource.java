cf3e095f9238ecbb422ddcae56dd23c6117da39c
vedanshugarg04 <vedanshu.garg@cloudsufi.com>
2026-06-29 17:57:00 +0530

    Add TRANSACTION_ISOLATION_LEVEL config in cloudsql-postgres and cloudsql-mysql plugins for develop
    fix: updated widget and docs
    
    fix: typos
    
    Added a constant for default isolation level

--- cloudsql-mysql-plugin/src/main/java/io/cdap/plugin/cloudsql/mysql/CloudSQLMySQLSource.java
+++ cloudsql-mysql-plugin/src/main/java/io/cdap/plugin/cloudsql/mysql/CloudSQLMySQLSource.java
@@ -156,6 +156,13 @@ public class CloudSQLMySQLSource extends AbstractDBSource<CloudSQLMySQLSource.Cl
     @Description("The existing connection to use.")
     private CloudSQLMySQLConnectorConfig connection;
 
+    @Name(TRANSACTION_ISOLATION_LEVEL)
+    @Description("Transaction isolation level for queries run by this source.")
+    @Nullable
+    private String transactionIsolationLevel;
+
+    private static final String DEFAULT_TRANSACTION_ISOLATION_LEVEL = "TRANSACTION_REPEATABLE_READ";
+
     @Override
     protected Map<String, String> getDBSpecificArguments() {
       if (getFetchSize() == null || getFetchSize() <= 0) {
@@ -166,6 +173,11 @@ public class CloudSQLMySQLSource extends AbstractDBSource<CloudSQLMySQLSource.Cl
       // statement will use cursor-based fetching to retrieve rows
       arguments.put("useCursorFetch", "true");
       return arguments;
+    }
+
+    @Override
+    public String getTransactionIsolationLevel() {
+      return transactionIsolationLevel != null ? transactionIsolationLevel : DEFAULT_TRANSACTION_ISOLATION_LEVEL;
     }
 
     @Override
